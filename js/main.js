db735936f7f4a822836d073be481ec940e95873b
Jessica <jesscara.993@gmail.com>
2022-11-14 14:38:54 +0100

    loop for defined

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
+
+const listaSpesa = [
+  'latte',
+  'uova',
+  'pane',
+  'pasta'
+];
+
+console.log(listaSpesa);
+
+for (let i = 0; i < listaSpesa.length; i++){
+  console.log(listaSpesa[i]);
+}
