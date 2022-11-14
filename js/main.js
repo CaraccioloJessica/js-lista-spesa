339fcae659f0aa8547d53e9b37219a4b28ebc646
Jessica <jesscara.993@gmail.com>
2022-11-14 14:41:33 +0100

    while loop defined

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
 
+// ARRAY CON LISTA SPESA
 const listaSpesa = [
   'latte',
   'uova',
@@ -9,6 +10,15 @@ const listaSpesa = [
 
 console.log(listaSpesa);
 
-for (let i = 0; i < listaSpesa.length; i++){
+// LOOP FOR CON STAMPA SINGOLI ITEMS
+// for (let i = 0; i < listaSpesa.length; i++){
+//   console.log(listaSpesa[i]);
+// }
+
+// LOOP WHILE 
+i = 0;
+while(i < listaSpesa.length){
   console.log(listaSpesa[i]);
+  i++;
 }
+
